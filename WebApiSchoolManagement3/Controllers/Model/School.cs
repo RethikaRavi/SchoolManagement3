d351eb2f6050c75b756b79bd1f904fbf64aefff4
deepikarethika <deepikasridhar2001@gmail.com>
2024-02-07 18:53:10 +0530

    changes

---
+++ WebApiSchoolManagement3/Controllers/Model/School.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiSchoolManagement3.Controllers.Model
+{
+    [Table("School")]
+    public class School
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Subjects { get; set; }
+        public int Marks { get; set; }
+    }
+}
---
+++ WebApiSchoolManagement3/Data/SchoolDbcontextSchoolManagement3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiSchoolManagement3.Controllers.Model;
+
+namespace WebApiSchoolManagement3.Data
+{
+    public class SchoolDbcontextSchoolManagement3Context : DbContext
+    {
+        public SchoolDbcontextSchoolManagement3Context (DbContextOptions<SchoolDbcontextSchoolManagement3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiSchoolManagement3.Controllers.Model.School> School { get; set; } = default!;
+    }
+}
